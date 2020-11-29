408710966392d4e9418eb8f9abe9418fe905aac3
918spyderporsche <ruishi4@illinois.edu>
2020-11-29 16:36:01 -0600

    insert movies into mongodb

--- mongo.py
+++ mongo.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import pymongo
+# from insertToMongo import outputJson
+import insertToMongo
 # import db
 mongo = Flask(__name__)
 @mongo.route('/')
@@ -9,17 +11,20 @@ def cluster0():
 @mongo.route('/test')
 def test():
     print("i'm here")
-    CONNECTION_STRING = "mongodb+srv://luowei:1124@cluster0.hckie.mongodb.net/users?retryWrites=true&w=majority"
+    CONNECTION_STRING = "mongodb+srv://shirui:pig@cluster0.hckie.mongodb.net/users?retryWrites=true&w=majority"
     client = pymongo.MongoClient(CONNECTION_STRING)
     db = client.users
     print(db.users_flask.count_documents({}))
     try:
-        db.users_flask.insert_one({"name": "John"})
-    except:
-        print("something is wrong")
+        movies = insertToMongo.outputJson()
+        # print(movies)
+        db.users_flask.insert_many(movies)
+    except Exception as e:
+        print(e)
     return "Connected to the data base!"
 
 if __name__ == '__main__':
     mongo.run(port=8000)
+    # mongo.debug = True
 #test to insert data to the data base
 
